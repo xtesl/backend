81515c656610885fc5cd927d4a68a43163287dcf
xtesl <146335490+xtesl@users.noreply.github.com>
2024-11-30 22:22:05 +0000

    Set up CORS

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.api.main import api_router
 from src.core.db import init_db
-# from app.core.config import settings
+from src.core.config import settings
 
 app = FastAPI(debug=True)
 
 # Create database tables
 # init_db()
-
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+  
 app.include_router(api_router)
